614d01973a108417ca719198b418fc2716a97832
vanessaamirandaa <149440221+vanessaamirandaa@users.noreply.github.com>
2024-10-28 00:24:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
 
+        window.scrollTo({
+            top: targetElement.offsetTop - 60, // adjust for the navbar height
+            behavior: 'smooth'
+        });
+    });
+});
